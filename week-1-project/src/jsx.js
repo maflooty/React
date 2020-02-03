933b975ba7a23cbd0eaf98f5bb932f4b00c08d80
Maflooty <mmaflooty@gmail.com>
2020-02-03 10:10:33 +0100

    jsx.js done

--- week-1-project/src/jsx.js
+++ week-1-project/src/jsx.js
@@ -6,10 +6,10 @@ function App() {
 
     return (
         <section>
-            <h1 className={"title"}>{title}</h1>
-            <p data-testid="injection">{'<script>alert("danger");</script>'}</p>
+            <h1 className={""}>{title}</h1>
+            <p data-testid="injection">{''}</p>
             <form>
-                <label data-testid="label" htmlFor="input-demo">Demo:</label>
+                <label data-testid="label" htmlFor="">Demo:</label>
                 <input data-testid="input" type="text" id="input-demo"/>
             </form>
         </section>
