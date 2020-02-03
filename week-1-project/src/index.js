412f04af5b548b0c220da64220a8183bd2976cdc
Maflooty <mmaflooty@gmail.com>
2020-02-03 10:53:46 +0100

    index.js done

--- week-1-project/src/index.js
+++ week-1-project/src/index.js
@@ -4,4 +4,4 @@ import { App } from './props'
 
 // Check the tests in ../public/index.html to see what to fix.
 
-ReactDOM.render(null, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
