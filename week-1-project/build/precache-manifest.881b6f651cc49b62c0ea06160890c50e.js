2a680c9ebe14466b774c2faac94da1045b575be8
Maflooty <mmaflooty@gmail.com>
2020-02-04 08:33:50 +0100

    build files

---
+++ week-1-project/build/precache-manifest.881b6f651cc49b62c0ea06160890c50e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23f088828f8946fc3f75deac1372b364",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/index.html"
+  },
+  {
+    "revision": "873f856c73b73935d50c",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/static/js/2.c6bafbf7.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/static/js/2.c6bafbf7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ccd5d563274fc51fd20c",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/static/js/main.8b56d8ff.chunk.js"
+  },
+  {
+    "revision": "338f801eb80f6348d8cd",
+    "url": "%3Cusername%3E.github.io/React/week-1-project/build/static/js/runtime-main.bc4eb59e.js"
+  }
+]);
